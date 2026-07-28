d8d6c6269d0d490fc5c1c44c027129ca76336643
Sanchay <127714519+Sanchay1304@users.noreply.github.com>
2026-07-28 15:26:50 +0530

    Update Task-1 - todo.py

--- Task-1 - todo.py
+++ Task-1 - todo.py
@@ -1,1 +1,30 @@
+# Simple To-Do List
 
+tasks = []
+
+while True:
+    print("\n1. Add Task")
+    print("2. View Tasks")
+    print("3. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        task = input("Enter the task: ")
+        tasks.append(task)
+        print("Task added.")
+
+    elif choice == "2":
+        if len(tasks) == 0:
+            print("No tasks available.")
+        else:
+            print("Your Tasks:")
+            for task in tasks:
+                print("-", task)
+
+    elif choice == "3":
+        print("Program closed.")
+        break
+
+    else:
+        print("Invalid choice.")
